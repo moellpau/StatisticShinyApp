14e4264ff17e5d13a3dfe8004118f3785408016b
Moeller <paula.moeller@sap.com>
2022-01-02 19:21:12 +0100

    aktueller Stand

--- test/app.R
+++ test/app.R
@@ -70,25 +70,35 @@ if (interactive()) {
     
     # Show a plot of the generated distribution
     dashboardBody(
+      fluidRow (
+      box(
+        title = "Was sind Konfidenzintervalle?",
+        "Unter Konfidenzintervallen (KI) sind ein statistische Intervalle zu verstehen, mit welchem man besser einschätzen kann, wo beispielsweise wie in diesem Fall der wahre Mittelwert eines Datensatzes liegt. Dieses Konzept wird angewendet, da in der Statistik berechnete Werte oft auf der Grundlage einer Stichprobe zustande kommen."
+      ),
+      box(
+        title = "Erklärung der Berechnung",
+        "Erklärung der Berechnung"
+      )),
+      fluidRow (
       infoBox(
-        "Was sind Konfidenzintervalle (KI)",
-        "text",
-        icon = icon("info"),
+        "Das Konfidenzniveau ist",
+        textOutput("konfidenzniveau"),
+        icon = icon("percentage"),
         color = "navy"
       ),
       infoBox(
-        "Erklärung der Berechnung",
-        "Test",
-        icon = icon("calculator"),
+        "Die Untergrenze des Konfidenzintervalls ist",
+        textOutput("intervall_a"),
+        icon = icon("step-backward"),
         color = "navy"
       ),
       infoBox(
-        "Verbale Erklärung",
-        "Test",
-        icon = icon("comments"),
+        "Die Obergrenze des Konfidenzintervalls ist",
+        textOutput("intervall_b"),
+        icon = icon("step-forward"),
         color = "navy"
-      ),
-      
+      )),
+      fluidRow(
       box(
         title = "Datensatz",
         solidHeader = TRUE,
@@ -99,21 +109,22 @@ if (interactive()) {
         title = "Konfidenzintervall",
         solidHeader = TRUE,
         status = "primary",
-        plotOutput("intervalldiagram"),
-        textOutput("quantile")
+        plotOutput("intervalldiagram")
+      )),
+      fluidRow (
+      box(
+        title = "Verbale Erklärung",
+        "Der wahre Mittelwert der Stichprobe von <n> Werten befindet sich mit einer Wahrscheinlichkeit von <niveau> % (Konfidenzniveau) innerhalb des Intervalls <aintervall> und <bintervall>",
       ),
-      infoBox(
-        "Quellen",
-        "https://jonasschemmel.shinyapps.io/Konfidenzintervall-Rechner/",
-        icon = icon("quote-right"),
-        color = "navy"
-      ),
+      box(
+        title = "Quellen",
+        "StudyFlix (2021): Konfidenzintervalle, in: https://studyflix.de/statistik/konfidenzintervall-1580, (Stand: 28.12.2021)
+        Schemmel, J. & Ziegler, M. (2020). Der Konfidenzintervall-Rechner: Web-Anwendung zur Berechnung und grafischen Darstellung von Konfidenzintervallen für die testpsychologische Diagnostik. Report Psychologie, 45(1), 16-21."
+      ))
       
     )
     
   )
-  
-  #g_data <- NULL
   
   # Define server logic required to draw a histogram
   server <- function(input, output, session) {
@@ -121,39 +132,42 @@ if (interactive()) {
       n <- input$n
       ewert <- input$ewert
       sd <- input$sd
-      xsd <- sd/sqrt(n)
-      x <- rnorm(n, ewert, sd)
+      x.values <- rnorm(n, ewert, sd)
+      y.values <- dnorm(x.values, ewert, sd)
       niveau <- input$niveau
+      niveau_percentage <- niveau * 100
       alpha <- 1 - niveau
-      z <- qnorm(1-(alpha/2), ewert, sd)
-      aintervall <- mean(x) - z*(xsd)
-      bintervall <- mean(x) + z*(xsd)
-      data <- data.frame(x, y = dnorm(x, ewert, sd))
-      print(aintervall)
-      print(bintervall)
-      t.test(x)
+      quantil <- 1-(alpha/2)
+      z <- qnorm(quantil, 0, 1)
+      f <- z * sd/sqrt(n)
+      aintervall <- mean(x.values) - f
+      bintervall <- mean(x.values) + f
     
     
     output$datadiagram <- renderPlot({
       # generate bins based on input$bins from ui.R
-      ggplot(data = data,
-             aes(x)) + geom_histogram (color="blue", fill = 'grey') + ylab("Häufigkeit") + xlab("Werte")
+      ggplot(NULL,
+             aes(x = x.values)) + geom_histogram (color="blue", fill = 'grey') + ylab("Häufigkeit") + xlab("Werte")
       # draw the histogram with the specified number of bins
       
     })
     output$intervalldiagram <- renderPlot({
       # generate bins based on input$bins from ui.R
-      ggplot(data = data,
-             aes(x, y)) + geom_line() +
-        geom_vline(aes(xintercept= aintervall), color="red", linetype="dashed", size=1) +
-        geom_vline(aes(xintercept= bintervall), color="blue", linetype="dashed", size=1) + 
-        #geom_area(data = data, aes(y = y), fill = 'cornflowerblue', color = NA, alpha = .3) + 
-        ylab("Dichte") + xlab("Werte")
+      ggplot(NULL,aes(x = x.values, y = y.values)) + geom_line() + labs(x="Werte", y=NULL) +
+        scale_y_continuous(breaks = NULL) +
+        scale_x_continuous(breaks = c(ewert - 2 * sd,
+                                      ewert - sd, ewert,
+                                      ewert + sd, ewert + 2 * sd)) +
+        geom_vline(aes(xintercept = aintervall)) +
+        geom_vline(aes(xintercept = bintervall))
+        #geom_area(data = data, aes(y = y), fill = 'cornflowerblue', color = NA, alpha = .3) 
 
       # draw the histogram with the specified number of bins
       
     })
-    output$quantile <- renderText("Verteilung von")
+    output$intervall_a <- renderText(aintervall)
+    output$intervall_b <- renderText(bintervall)
+    output$konfidenzniveau <- renderText(niveau_percentage)
     
     })
     observeEvent(input$normierung, {
