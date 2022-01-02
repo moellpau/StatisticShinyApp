3f723dd02c6cb0d646b2da393950b4ffe20d927d
Marby <friederike.marby@sap.com>
2022-01-02 21:49:12 +0100

    button umbenannt

--- test/app.R
+++ test/app.R
@@ -63,64 +63,64 @@ if (interactive()) {
         min = 1,
         max = 20
       ),
-      actionButton(
-        "button", "Verteile mich!"
-      )
+      actionButton("verteil_mich_button", "Verteile mich!")
     ),
     
     # Show a plot of the generated distribution
     dashboardBody(
       fluidRow (
-      box(
-        title = "Was sind Konfidenzintervalle?",
-        "Unter Konfidenzintervallen (KI) sind ein statistische Intervalle zu verstehen, mit welchem man besser einschätzen kann, wo beispielsweise wie in diesem Fall der wahre Mittelwert eines Datensatzes liegt. Dieses Konzept wird angewendet, da in der Statistik berechnete Werte oft auf der Grundlage einer Stichprobe zustande kommen."
-      ),
-      box(
-        title = "Erklärung der Berechnung",
-        "Erklärung der Berechnung"
-      )),
-      fluidRow (
-      valueBox(
-        textOutput("konfidenzniveau"),
-        p("Konfidenzniveau"),
-        icon = icon("flag-checkered"),
-        color = "light-blue"
-      ),
-      valueBox(
-        textOutput("intervall_a"),
-        "Untergrenze des Konfidenzintervalls",
-        icon = icon("less-than-equal"),
-        color = "olive"
-      ),
-      valueBox(
-        textOutput("intervall_b"),
-        "Obergrenze des Konfidenzintervalls",
-        icon = icon("greater-than-equal"),
-        color = "olive"
-      )),
-      fluidRow(
-      box(
-        title = "Datensatz",
-        solidHeader = TRUE,
-        status = "primary",
-        plotOutput("datadiagram")
-      ),
-      box(
-        title = "Konfidenzintervall",
-        solidHeader = TRUE,
-        status = "primary",
-        plotOutput("intervalldiagram")
-      )),
-      fluidRow (
-      box(
-        title = "Verbale Erklärung",
-        textOutput("verbal_erkl")
+        box(
+          title = "Was sind Konfidenzintervalle?",
+          "Unter Konfidenzintervallen (KI) sind ein statistische Intervalle zu verstehen, mit welchem man besser einschätzen kann, wo beispielsweise wie in diesem Fall der wahre Mittelwert eines Datensatzes liegt. Dieses Konzept wird angewendet, da in der Statistik berechnete Werte oft auf der Grundlage einer Stichprobe zustande kommen."
         ),
-      box(
-        title = "Quellen",
-        "StudyFlix (2021): Konfidenzintervalle, in: https://studyflix.de/statistik/konfidenzintervall-1580, (Stand: 28.12.2021)
+        box(title = "Erklärung der Berechnung",
+            textOutput("berech_erkl"))
+      ),
+      
+      
+      fluidRow (
+        valueBox(
+          textOutput("konfidenzniveau"),
+          p("Konfidenzniveau"),
+          icon = icon("flag-checkered"),
+          color = "light-blue"
+        ),
+        valueBox(
+          textOutput("intervall_a"),
+          "Untergrenze des Konfidenzintervalls",
+          icon = icon("less-than-equal"),
+          color = "olive"
+        ),
+        valueBox(
+          textOutput("intervall_b"),
+          "Obergrenze des Konfidenzintervalls",
+          icon = icon("greater-than-equal"),
+          color = "olive"
+        )
+      ),
+      fluidRow(
+        box(
+          title = "Datensatz",
+          solidHeader = TRUE,
+          status = "primary",
+          plotOutput("datadiagram")
+        ),
+        box(
+          title = "Konfidenzintervall",
+          solidHeader = TRUE,
+          status = "primary",
+          plotOutput("intervalldiagram")
+        )
+      ),
+      fluidRow (
+        box(title = "Verbale Erklärung",
+            textOutput("verbal_erkl")),
+        box(
+          title = "Quellen",
+          "StudyFlix (2021): Konfidenzintervalle, in: https://studyflix.de/statistik/konfidenzintervall-1580, (Stand: 28.12.2021)
         Schemmel, J. & Ziegler, M. (2020). Der Konfidenzintervall-Rechner: Web-Anwendung zur Berechnung und grafischen Darstellung von Konfidenzintervallen für die testpsychologische Diagnostik. Report Psychologie, 45(1), 16-21."
-      ))
+        )
+      )
       
     )
     
@@ -128,7 +128,8 @@ if (interactive()) {
   
   # Define server logic required to draw a histogram
   server <- function(input, output, session) {
-    observeEvent(input$button, {
+    
+    observeEvent(input$verteil_mich_button, {
       n <- input$n
       ewert <- input$ewert
       sd <- input$sd
@@ -137,52 +138,77 @@ if (interactive()) {
       niveau <- input$niveau
       niveau_percentage <- niveau * 100
       alpha <- 1 - niveau
-      quantil <- 1-(alpha/2)
+      quantil <- 1 - (alpha / 2)
       z <- qnorm(quantil, 0, 1)
-      f <- z * sd/sqrt(n)
+      f <- z * sd / sqrt(n)
       aintervall <- mean(x.values) - f
       bintervall <- mean(x.values) + f
-    
-    
-    output$datadiagram <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      ggplot(NULL,
-             aes(x = x.values)) + geom_histogram (color="blue", fill = 'grey') + ylab("Häufigkeit") + xlab("Werte")
-      # draw the histogram with the specified number of bins
       
-    })
-    output$intervalldiagram <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      ggplot(NULL,aes(x = x.values, y = y.values)) + geom_line() + labs(x="Werte", y=NULL) +
-        scale_y_continuous(breaks = NULL) +
-        scale_x_continuous(breaks = c(ewert - 2 * sd,
-                                      ewert - sd, ewert,
-                                      ewert + sd, ewert + 2 * sd)) +
-        geom_vline(aes(xintercept = aintervall)) +
-        geom_vline(aes(xintercept = bintervall))
-        #geom_area(data = data, aes(y = y), fill = 'cornflowerblue', color = NA, alpha = .3) 
-
-      # draw the histogram with the specified number of bins
       
-    })
-   
-    output$intervall_a <- renderText({ 
-      paste(round(aintervall, digits = 2))
-    })
-
-    output$intervall_b <- renderText({ 
-      paste(round(bintervall, digits = 2))
-    })
-      
-    output$konfidenzniveau <- renderText({ 
-        paste(niveau_percentage, "%") 
-    })
-    
-    output$verbal_erkl <- renderText({ 
-      paste("Der wahre Mittelwert der Stichprobe von ", n, "Werten befindet sich mit einer Wahrscheinlichkeit von",  niveau_percentage, "% innerhalb des Intervalls", round(aintervall, digits = 2), "bis", round(bintervall, digits = 2), ".") 
-    })
-  
+      output$datadiagram <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        ggplot(NULL,
+               aes(x = x.values)) + geom_histogram (color = "blue", fill = 'grey') + ylab("Häufigkeit") + xlab("Werte")
+        # draw the histogram with the specified number of bins
+        
       })
+      output$intervalldiagram <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        ggplot(NULL, aes(x = x.values, y = y.values)) + geom_line() + labs(x =
+                                                                             "Werte", y = NULL) +
+          scale_y_continuous(breaks = NULL) +
+          scale_x_continuous(breaks = c(ewert - 2 * sd,
+                                        ewert - sd, ewert,
+                                        ewert + sd, ewert + 2 * sd)) +
+          geom_vline(aes(xintercept = aintervall)) +
+          geom_vline(aes(xintercept = bintervall))
+        #geom_area(data = data, aes(y = y), fill = 'cornflowerblue', color = NA, alpha = .3)
+        
+        # draw the histogram with the specified number of bins
+        
+      })
+      
+      output$intervall_a <- renderText({
+        paste("a =",round(aintervall, digits = 2))
+      })
+      
+      output$intervall_b <- renderText({
+        paste("b =", round(bintervall, digits = 2))
+      })
+      
+      output$konfidenzniveau <- renderText({
+        paste(niveau_percentage, "%")
+      })
+      
+      output$berech_erkl <- renderText({
+        paste(
+          "Für die Findung und Validierung der Intervalle suchen wir einen Intervall von der Untergrenze a bis zur Obergrenze b.",
+          "Die Grenzen dieses Intervalles sollen so ermittelt werden, dass mit",
+          niveau_percentage,
+          "%-iger Wahrscheinlichkeit der echte Mittelwert der Grundgesamtheit in ihnen liegt.",
+          "Dabei ist die Annahme, dass der Intervall symmetrisch und die Werte immer normalverteilt sind und wir die Standardabweichung, in diesem Fall",
+          sd,
+          "kennen."
+        )
+      })
+      
+      output$verbal_erkl <- renderText({
+        paste(
+          "Der wahre Mittelwert der Stichprobe von ",
+          n,
+          "Werten befindet sich mit einer Wahrscheinlichkeit von",
+          niveau_percentage,
+          "% innerhalb des Intervalls",
+          round(aintervall, digits = 2),
+          "bis",
+          round(bintervall, digits = 2),
+          "."
+        )
+      })
+      
+    })
+    
+    
     observeEvent(input$normierung, {
       if (input$normierung == 1) {
         updateNumericInput(session, "ewert", value = 100)
