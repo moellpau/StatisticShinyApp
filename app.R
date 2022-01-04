d70a67c7f82fb8ee650a1abcdc39624de0bd8df0
Moeller <paula.moeller@sap.com>
2022-01-04 09:16:12 +0100

    versucht Formeln einzupflegen (muss nochmal bearbeitet werden) kleine Änderungen ggplot

--- app.R
+++ app.R
@@ -86,7 +86,13 @@ if (interactive()) {
           div(HTML("Unter Konfidenzintervallen (KI) sind ein statistische Intervalle zu verstehen, mit welchem man 
                     besser einschätzen kann, wo beispielsweise wie in diesem Fall der wahre Mittelwert eines Datensatzes liegt. 
                    Dieses Konzept wird angewendet, da in der Statistik berechnete Werte oft auf der Grundlage einer Stichprobe zustande kommen.
-                   <br> Die Formeln für die Ober- und Untergrenze sind:"
+                   <br> Die Formeln für die Ober- und Untergrenze sind: <br>
+                   <math> a = X̅ - 
+                    <msub>
+                      <mi>Z</mi>
+                      <mn>1 - α/2</mn>
+                    </msub>
+                   *  <mfrac>σ <msqrt> n </msqrt></mfrac></math>"
         ))),
         box(title = "Erklärung der Berechnung",
             uiOutput("berech_erkl"))
@@ -160,25 +166,22 @@ if (interactive()) {
       
       
       output$datadiagram <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        ggplot(NULL,
-               aes(x = x.values)) + geom_histogram (color = "blue", fill = 'grey') + ylab("Häufigkeit") + xlab("Werte")
-        # draw the histogram with the specified number of bins
+        ggplot(NULL, aes(x = x.values)) + 
+          # draw the histogram with the specified number of bins
+          geom_histogram (binwidth = 5, color = "#3C8DBC", fill = 'grey') + ylab("Häufigkeit") + xlab("Werte")
+        
         
       })
       output$intervalldiagram <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        ggplot(NULL, aes(x = x.values, y = y.values)) + geom_line() + labs(x =
-                                                                             "Werte", y = NULL) +
+        ggplot(NULL, aes(x = x.values, y = y.values)) + 
+          geom_line() + labs(x = "Werte", y = NULL) +
           scale_y_continuous(breaks = NULL) +
           scale_x_continuous(breaks = c(ewert - 2 * sd,
                                         ewert - sd, ewert,
                                         ewert + sd, ewert + 2 * sd)) +
-          geom_vline(aes(xintercept = aintervall)) +
-          geom_vline(aes(xintercept = bintervall))
-        #geom_area(data = data, aes(y = y), fill = 'cornflowerblue', color = NA, alpha = .3)
-        
-        # draw the histogram with the specified number of bins
+          geom_vline(aes(xintercept = aintervall), color = "royalblue1") +
+          geom_vline(aes(xintercept = bintervall), color = "red") +
+         geom_area(data = NULL, aes(y = y.values), fill = "#3C8DBC", color = NA, alpha = .3)
         
       })
       
@@ -202,7 +205,8 @@ if (interactive()) {
       output$berech_erkl <- renderUI({
         tagList(
           p(
-            "Für die Findung und Validierung der Intervalle suchen wir einen Intervall von der Untergrenze",
+            "Für eine erste Vorauswahl können über das Drop-Down erste Werte für Erwartungswert und Standardabweichung von Normskalen festgelegt werden.
+            Für die Findung und Validierung der Intervalle suchen wir einen Intervall von der Untergrenze",
             em("a"),
             "bis zur Obergrenze",
             em("b"),
@@ -240,10 +244,10 @@ if (interactive()) {
          div(HTML(
          "<ul><li>StudyFlix (2021): Konfidenzintervalle, in: https://studyflix.de/statistik/konfidenzintervall-1580, (Stand: 28.12.2021) </li>
         <li> Schemmel, J. & Ziegler, M. (2020). Der Konfidenzintervall-Rechner: Web-Anwendung zur Berechnung und grafischen Darstellung von Konfidenzintervallen für die testpsychologische Diagnostik. Report Psychologie, 45(1), 16-21. </li>
-        <li> https://rdrr.io/cran/shinyjs/ </li>
-        <li> https://rstudio.github.io/shinydashboard/structure.html </li>
-        <li> Schmuller, J. (2017): Statistik mir R für Dummies, Weinheim. </li>
-        <li> https://de.wikipedia.org/wiki/Normwertskala </li><ul>"
+        <li> Rdrr.iO (2022): shinyjs, in: https://rdrr.io/cran/shinyjs/, (Stand: 04.01.2022). </li>
+        <li> RStudio (2022): ShinyDashboard, in: https://rstudio.github.io/shinydashboard/structure.html, (Stand: 04.01.2022). </li>
+        <li> Schmuller, J. (2017): Statistik mit R für Dummies, Weinheim. </li>
+        <li> Wikipedia (2022): Normwertskala, in: https://de.wikipedia.org/wiki/Normwertskala, (Stand: 04.01.2022). </li><ul>"
           ))
         )
       })
